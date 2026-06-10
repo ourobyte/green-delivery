64c900e025c818b191ba53f6092c45e7ee21799c
ourobyte <firbatwentyfourindo@gmail.com>
2026-06-10 11:31:21 +0700

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "Delivery Manager",
+  "short_name": "Delivery",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#121212",
+  "theme_color": "#317873",
+  "icons": [
+    {
+      "src": "https://via.placeholder.com/192/317873/FFFFFF?text=DM",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
